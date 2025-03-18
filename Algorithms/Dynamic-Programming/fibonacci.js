f0cc8504b0e2277a66fa930d1c4fc57662aa3d17
Shahan Khan <prince47khan@gmail.com>
2025-03-18 21:33:36 +0530

    iterative dynamic programming

--- Algorithms/Dynamic-Programming/fibonacci.js
+++ Algorithms/Dynamic-Programming/fibonacci.js
@@ -26,11 +26,33 @@ function fibMaster() {
     }
 }
 
+//Very Slow
 const fasterFib = fibMaster();
 console.log(new Date())
 console.log(fib(41))            // Don't go above 41 - system will crash
 console.log(new Date())
 
+//Very fast
 console.log(new Date())
 console.log(fasterFib(75))
+console.log(new Date())
+
+
+// There's another way to solve this using Iterative-Dynamic-Programming approach:
+function fibIterative(n) {
+    if (n<2) {
+        return n
+    }
+    else {
+        let answer = [0,1];
+        for (let i=2; i<=n; i++) {
+            answer.push(answer[i-2] + answer[i-1])
+        }
+        return answer.pop()
+    }
+}
+
+//Very fast
+console.log(new Date())
+console.log(fibIterative(75))
 console.log(new Date())
